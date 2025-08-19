07873524cf9e8b8497df3f94cff3b53e53de3b37
akul <nurislamimanudin@gmail.com>
2025-08-19 21:55:32 +0700

    re-export Level logging

--- lib/easy_admob_ads_flutter.dart
+++ lib/easy_admob_ads_flutter.dart
@@ -18,3 +18,4 @@ export 'src/widgets/admob_rewarded_interstitial_ad.dart';
 // Package Exports
 export 'package:google_mobile_ads/google_mobile_ads.dart';
 export 'package:shared_preferences/shared_preferences.dart';
+export 'package:logging/logging.dart' show Level;
